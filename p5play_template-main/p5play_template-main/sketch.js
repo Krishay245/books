bbd04c16f66616fd51ebe2e074c470de2fd6d7e6
Krishay245 <132805294+Krishay245@users.noreply.github.com>
2023-05-07 10:13:25 +0400

    Add files via upload

---
+++ p5play_template-main/p5play_template-main/sketch.js
@@ -0,0 +1,21 @@
+var ball 
+function setup() {
+  createCanvas(400,400);
+
+  ball = createSprite(100,200,50,100)
+
+  ball.shapeColor = "red"
+}
+
+function draw() 
+{
+  background(30);
+
+  drawSprites()
+
+
+}
+
+
+
+
